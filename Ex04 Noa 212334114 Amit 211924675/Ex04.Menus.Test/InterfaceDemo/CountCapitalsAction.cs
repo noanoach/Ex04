b19cf30f32ed4e04ab765b2f423aea6ce2c6059d
noa.noach <noa.noach@post.runi.ac.il>
2026-06-12 15:00:37 +0300

    added more files in Tests

--- Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/CountCapitalsAction.cs
+++ Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/CountCapitalsAction.cs
@@ -1,10 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Ex04.Menus.Interfaces.Interfaces;
 
 namespace Ex04.Menus.Test.InterfaceDemo
 {
-    internal class CountCapitalsAction
+    // Counts uppercase letters in text entered by the user.
+    public class CountCapitalsAction : IMenuItemSelectedListener
     {
+        public void MenuItemSelected()
+        {
+            string userText = Console.ReadLine();
+            int uppercaseLettersCount = countUppercaseLetters(userText);
+
+            Console.WriteLine("> There are {0} uppercase letters in your text", uppercaseLettersCount);
+        }
+
+        private int countUppercaseLetters(string i_Text)
+        {
+            int uppercaseLettersCount = 0;
+
+            foreach (char currentChar in i_Text)
+            {
+                if (char.IsUpper(currentChar))
+                {
+                    ++uppercaseLettersCount;
+                }
+            }
+
+            return uppercaseLettersCount;
+        }
     }
 }
--- Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/ShowTimeAction.cs
+++ Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/ShowTimeAction.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Ex04.Menus.Interfaces.Interfaces;
 
 namespace Ex04.Menus.Test.InterfaceDemo
 {
-    internal class ShowTimeAction
+    // Displays the current time when selected from the interfaces menu.
+    public class ShowTimeAction : IMenuItemSelectedListener
     {
+        public void MenuItemSelected()
+        {
+            Console.WriteLine("> Current Time is {0}", DateTime.Now.ToShortTimeString());
+        }
     }
 }
