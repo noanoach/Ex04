ba7886c839868f6fc53aa2e6a8f3554dc1e133d7
noa.noach <noa.noach@post.runi.ac.il>
2026-06-12 14:43:39 +0300

    added Tests.interfaceDemo and Program.cs

--- Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/ShowDateAction.cs
+++ Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/ShowDateAction.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Ex04.Menus.Interfaces.Interfaces;
 
 namespace Ex04.Menus.Test.InterfaceDemo
 {
-    internal class ShowDateAction
+    // Displays the current date when selected from the interfaces menu.
+    public class ShowDateAction : IMenuItemSelectedListener
     {
+        public void MenuItemSelected()
+        {
+            Console.WriteLine("> Current Date is {0}", DateTime.Now.ToShortDateString());
+        }
     }
 }
