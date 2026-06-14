83c03c5eb923b589e27a1e593f5d757b7c680784
noa.noach <noa.noach@post.runi.ac.il>
2026-06-14 12:48:39 +0300

    added program final touchs

---
+++ Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/InterfaceDemo/InterfaceMenuBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex04.Menus.Interfaces;
+using Ex04.Menus.Interfaces.MenuItems;
+using Ex04.Menus.Test.InterfaceDemo;
+
+namespace Ex04.Menus.Test.InterfaceDemo
+{
+    internal class InterfaceMenuBuilder
+    {
+
+        public static MainMenu buildInterfacesMenu()
+        {
+            MainMenu interfacesMainMenu = new MainMenu("Interfaces Main Menu");
+
+            MenuSubMenuItem showCurrentDateTimeSubMenu =
+                new MenuSubMenuItem("Show Current Date/Time");
+
+            MenuSubMenuItem versionAndCapitalsSubMenu =
+                new MenuSubMenuItem("Version and Capitals");
+
+            showCurrentDateTimeSubMenu.AddMenuItem(
+                new MenuActionItem("Show Current Date", new ShowDateAction()));
+
+            showCurrentDateTimeSubMenu.AddMenuItem(
+                new MenuActionItem("Show Current Time", new ShowTimeAction()));
+
+            versionAndCapitalsSubMenu.AddMenuItem(
+                new MenuActionItem("Count Capitals", new CountCapitalsAction()));
+
+            versionAndCapitalsSubMenu.AddMenuItem(
+                new MenuActionItem("Show Version", new ShowVersionAction()));
+
+            interfacesMainMenu.RootMenu.AddMenuItem(showCurrentDateTimeSubMenu);
+            interfacesMainMenu.RootMenu.AddMenuItem(versionAndCapitalsSubMenu);
+
+            return interfacesMainMenu;
+        }
+    }
+}
--- Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/Program.cs
+++ Ex04 Noa 212334114 Amit 211924675/Ex04.Menus.Test/Program.cs
@@ -1,6 +1,4 @@
-﻿using Ex04.Menus.Interfaces;
-using Ex04.Menus.Interfaces.MenuItems;
-using Ex04.Menus.Test.InterfaceDemo;
+﻿using Ex04.Menus.Test.InterfaceDemo;
 using Ex04.Menus.Test.DelegateDemo;
 
 namespace Ex04.Menus.Test
@@ -23,37 +21,12 @@ namespace Ex04.Menus.Test
 
         private static void runInterfacesMenu()
         {
-            MainMenu interfacesMainMenu = buildInterfacesMenu();
+            Ex04.Menus.Interfaces.MainMenu interfacesMainMenu = 
+                InterfaceMenuBuilder.buildInterfacesMenu();
 
             interfacesMainMenu.Show();
         }
 
-        private static MainMenu buildInterfacesMenu()
-        {
-            MainMenu interfacesMainMenu = new MainMenu("Interfaces Main Menu");
-
-            MenuSubMenuItem showCurrentDateTimeSubMenu =
-                new MenuSubMenuItem("Show Current Date/Time");
-
-            MenuSubMenuItem versionAndCapitalsSubMenu =
-                new MenuSubMenuItem("Version and Capitals");
-
-            showCurrentDateTimeSubMenu.AddMenuItem(
-                new MenuActionItem("Show Current Date", new ShowDateAction()));
-
-            showCurrentDateTimeSubMenu.AddMenuItem(
-                new MenuActionItem("Show Current Time", new ShowTimeAction()));
-
-            versionAndCapitalsSubMenu.AddMenuItem(
-                new MenuActionItem("Count Capitals", new CountCapitalsAction()));
-
-            versionAndCapitalsSubMenu.AddMenuItem(
-                new MenuActionItem("Show Version", new ShowVersionAction()));
-
-            interfacesMainMenu.RootMenu.AddMenuItem(showCurrentDateTimeSubMenu);
-            interfacesMainMenu.RootMenu.AddMenuItem(versionAndCapitalsSubMenu);
-
-            return interfacesMainMenu;
-        }
+       
     }
 }
